af42be5b0502b648e4a55d445510c57fb09956a1
nmhummel <natalie.hummel@gmail.com>
2020-10-21 17:37:42 -0500

    commit tree 6c2c656e525ae13515bd5fc3a4a39e2fffab2ca1

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,46 @@
+require 'pry'
+
+#1
 def create_an_empty_array
-  
+    []
 end
 
+#2
 def create_an_array
-  
+    create_an_array = ["red", "yellow", "black", "white"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+#3
+def add_element_to_end_of_array(array, element)   
+    array.push(element)
 end
 
+#4
 def add_element_to_start_of_array(array, element)
-  
+    array.unshift(element)
 end
 
+#5
 def remove_element_from_end_of_array(array)
-  
+    array.pop
 end
 
+#6
 def remove_element_from_start_of_array(array)
-  
+    array.shift 
 end
 
+#7
 def retrieve_element_from_index(array, index_number)
-  
+   array[index_number]
 end
 
+#8
 def retrieve_first_element_from_array(array)
-  
+    array.first
 end
 
+#9
 def retrieve_last_element_from_array(array)
-  
+    array[-1]
 end
